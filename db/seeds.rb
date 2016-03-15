199fa7af013fa547bb4718b6407b5429f51bd2fd
Rick Henry <rick.henry@codeclan.com>
2016-03-15 15:58:48 +0000

    added in events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
+require( 'pry-byebug' )
+require_relative( '../models/event' )
 
+event1 = Event.new( { 'start_time' => '2016-04-01 09:00:00', 'end_time' => '2016-04-02 09:00:00', 'title' => 'JS Conference', 'location' => 'London' } )
+
+binding.pry
+nil
