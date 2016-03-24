75bb28283a34c541f0174e972a64ae7fbd7c54d9
Rick Henry <rick.henry@codeclan.com>
2016-03-24 15:37:16 +0000

    update server

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require( 'sinatra' )
 require( 'pry-byebug' )
-require_relative('controllers/appointments_controller')
-require_relative('controllers/events_controller')
+require_relative( 'controllers/appointments_controller' )
+require_relative( 'controllers/events_controller' )
 
 
 get '/' do
