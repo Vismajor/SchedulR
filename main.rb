f428eda3460c660ba51f71d78359f6ea1d6494f6
Rick Henry <rick.henry@codeclan.com>
2016-03-29 16:02:59 +0100

    tweaks

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 require( 'sinatra' )
 require( 'pry-byebug' )
+require_relative( 'models/calendar' )
 require_relative( 'controllers/appointments_controller' )
 require_relative( 'controllers/events_controller' )
 
 
 get '/' do
+  options = {
+    'appointments' => Appointment.all()
+    'events' => Event.all()
+  }
+
+  @calendar = Calendar.new( options )
+
   erb :"home"
 end
